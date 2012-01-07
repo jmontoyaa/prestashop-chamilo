0ae0ffdb80ea2855239051bd985d3ced28c61f86
ywarnier <devnull@localhost>
2012-01-07 18:41:03 -0500

    Added French support

---
+++ chamilo/fr.php
@@ -0,0 +1,23 @@
+<?php
+/* For licensing terms, see /license.txt */
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{chamilo}prestashop>chamilo_e48627d53b0577b925bf7bd402a2e7f4'] = 'Module Chamilo 1.8.x';
+$_MODULE['<{chamilo}prestashop>chamilo_d7b7d7bb029a44b484922984d16bc2e7'] = 'Permet à vos utilisateurs d\'acheter des cours de Chamilo';
+$_MODULE['<{chamilo}prestashop>chamilo_f4f70727dc34561dfde1a3c529b6205c'] = 'Configuration';
+$_MODULE['<{chamilo}prestashop>chamilo_6df1f9b5662398a551a2c9185b26638a'] = 'Pour ajouter des produits sur la page d\'accueil, positionnez-les dans la catégorie "Accueil".';
+$_MODULE['<{chamilo}prestashop>chamilo_20c47700083df21a87e47d9299ef4dc4'] = 'Nombre de produits à afficher';
+$_MODULE['<{chamilo}prestashop>chamilo_ada7594fb1614c6b6d271699c8511d5e'] = 'Nombre de produits à afficher dans Accueil (par défaut: 10)';
+$_MODULE['<{chamilo}prestashop>chamilo_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{chamilo}prestashop>chamilo_890c01592fd630e35fdf7ac214642433'] = 'Configuration de Chamilo';
+$_MODULE['<{chamilo}prestashop>chamilo_fb6cf02593ab19f604e27186e83b2ce9'] = 'URL de Chamilo';
+$_MODULE['<{chamilo}prestashop>chamilo_24463a8c6d72e789cb1c9dfa931119fd'] = 'Chamilo WDLS';
+$_MODULE['<{chamilo}prestashop>chamilo_5f8e6091e34344211552b3643ef02ee3'] = 'Clef de sécurité de Chamilo';
+$_MODULE['<{chamilo}prestashop>chamilo_11632f2f2e8c131d0e7063884ae4ac47'] = 'Méthode d\'encryption de Chamilo';
+$_MODULE['<{chamilo}prestashop>chamilo_5f92992ef984d1e53985879ae3a73a76'] = 'IP publique';
+$_MODULE['<{chamilo}prestashop>chamilo_c93c2a114c04fa368085fd0e06a99c4a'] = 'Cours disponibles dans Chamilo';
+$_MODULE['<{chamilo}prestashop>chamilo_e012d6d70f28bce74a4eb7322daf0ce1'] = 'Titre - Code';
+$_MODULE['<{chamilo}prestashop>chamilo_a1ed9179244bd9920a93d0af8b864de9'] = 'Aucun cours trouvé dans Chamilo. Vous devez d\'abord en créer. Si vous les avez déjà créé, vérifiez la configuration de Chamilo';
+$_MODULE['<{chamilo}prestashop>chamilo_f4d1ea475eaa85102e2b4e6d95da84bd'] = 'Confirmer';
+$_MODULE['<{chamilo}prestashop>chamilo_c888438d14855d7d96a2724ee9c306bd'] = 'Configuration mise à jour';
+$_MODULE['<{chamilo}prestashop>chamilo_ced2b9f1aad1f8a5b351a3120e4b1212'] = 'Nombre de produits non valides';
